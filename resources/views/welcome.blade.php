7ec8c49214ab2f0f4d359b8e429f0a57001f3ad2
Ryan Helyer <r.hellyer@syde.com>
2025-12-05 21:10:19 +0100

    x

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,87 +1,45 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width" />
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
-<style>img:is([sizes="auto" i], [sizes^="auto," i]) { contain-intrinsic-size: 3000px 1500px }</style>
+@extends('layouts.app')
 
-<title>Spam Destruction - Spam Annhilator</title>
-<link rel="canonical" href="{{ url('/') }}" />
-<meta property="og:locale" content="{{ str_replace('_', '-', app()->getLocale()) }}" />
-<meta property="og:type" content="website" />
-<meta property="og:title" content="Spam Destruction - Spam Annhilator" />
-<meta property="og:description" content="This service hasn't proven very popular (partially due to lack of promotion) and so it has been deprecated. All old links will continue working however! Powered by the Spam Destroyer for WordPress" />
-<meta property="og:url" content="{{ url('/') }}" />
-<meta property="og:site_name" content="Spam Annhilator" />
-<meta name="twitter:card" content="summary_large_image" />
+@section('body-class', 'home')
+@section('page-title', 'Spam Annihilator')
+@section('page-tagline', 'Easily block spam from Discord, Telegram and other invite links')
 
-<link rel='dns-prefetch' href='//fonts.googleapis.com' />
-<link rel='stylesheet' id='spamannhilator-css' href='{{ asset('css/old.css') }}' type='text/css' media='all' />
-<link rel='stylesheet' id='google-open-sans-css' href='https://fonts.googleapis.com/css?family=Open+Sans%3A400%2C800&#038;ver=1.0925962461' type='text/css' media='all' />
-</head>
-<body class="home wp-singular page-template-default page">
-<a class="skip-link screen-reader-text" href="#main">Skip to content</a>
+@section('header-form')
+<p style="padding: 10rem 0;">We're under construction and will be ready soon!</p>
+@endsection
 
-<div id="cookie-notice" class="hidden"></div>
+@section('content') 
+  <section class="primary testimonials">
+    <div class="wrapper">
+        <h1>Testimonials</h1>
 
-<header id="site-header" role="banner">
-	<h1>Protect Links</h1>
-	<p>Easily block spam from Discord, Telegram and other invite links</p>
-</header>
+        <blockquote>
+            <img src="/images/craig-sailor.avif" width="300" height="300" alt="Craig Sailor">
+            <p>Stunning service! Spam made our large Telegram group unusable. Switching to Spam Annihilator instantly brought us zero spam. Members are thrilled!</p>
+            <cite>Craig Sailor – Berlin Coworking Club</cite>
+        </blockquote>
 
-<main id="main">
-<article id="main-content">
-<p>This service hasn't proven very popular (partially due to lack of promotion) and so it has been deprecated. All old links will continue working however!</p>
+        <blockquote>
+            <img src="/images/georg-preller.avif" width="300" height="300" alt="Georg Preller">
+            <p>Spam was ruining the Abenteuer Freundschaft - Wandern / Hiking group. Spam Annihilator gave us zero spam immediately. Highly recommended!</p>
+            <cite>Georg Preller – Abenteuer Freundschaft</cite>
+        </blockquote>
 
+        <blockquote>
+            <img src="/images/liubov-kurilova.avif" width="300" height="300" alt="Liubov Kurilova">
+            <p>Spammers constantly interrupted my DMs and comments, making it tough to connect with followers. Thanks to Spam Annihilator, my Kurilove_Art channel now has instant, zero-spam protection. I can finally focus on creating!</p>
+            <cite>Liubov Kurilova – Kurilove_Art</cite>
+        </blockquote>
+    </div>
+  </section>
 
-
-<section>
-<div class="inner">
-<h2>Powered by the Spam Destroyer for WordPress</h2>
-<p>The Spam Annihilator service is powered by the same technology protecting website comments via our popular Spam Destroyer plugin for WordPress on over 7,000 websites! <a href="https://geek.hellyer.kiwi/products/spam-destroyer/">The Spam Destroyer plugin</a> is intended to be effortless to use. Simply install, and enjoy a spam free website. Idea for those of you who use WordPress to power your website.</p>
-<a class="button" href="https://geek.hellyer.kiwi/products/spam-destroyer/">Download plugin</a></div>
-</section>
-
-
-
-<blockquote class="wp-block-quote testimonial is-layout-flow wp-block-quote-is-layout-flow">
-<p>Stunning service! As our group grew into the thousands, the spammers kept coming in to the point where our Telegram group was barely usable. But since switching to a Spam Annihilator link, we have seen no more spammers. Our group members are ecstatic to be able to continue using the group without having to deal with a constant bombarment of annoying ads.</p>
-<cite>Craig Sailor – head adminstrator of Coworking Berlin</cite></blockquote>
-
-
-
-<section>
-<div class="inner">
-<h2>Custom Spam Solutions</h2>
-<p>If you require a custom anti-spam solution, either for a website, specific type of URLS, commenting systems etc. please get in touch. We have a proven track record in in solving tough spam problems and can help you get rid of spam once and for all.</p>
-<a class="button" href="https://ryan.hellyer.kiwi/contact/">Contact us</a></div>
-</section>
-</article>
-</main><!-- #main -->
-
-
-<footer id="site-footer" role="contentinfo">
-
-	<div class="wrap">
-
-		<aside class="widget"><h3>Spam Annihilator</h3>			<div class="textwidget"><p>A powerful system for blocking spammers on Discord, Telegram and other services. Powered by the internals of the Spam Destroyer plugin for WordPress.</p>
-<ul>
-<li><a href="https://ryan.hellyer.kiwi/contact/">Contact Us</a></li>
-</ul>
-</div>
-		</aside><aside class="widget"><h3>Product of Ryan Hellyer</h3>			<div class="textwidget"><p>WordPress developer for hire. Contact Ryan for custom plugins, themes and extra functionality in WordPress.</p>
-<ul>
-<li><a href="https://geek.hellyer.kiwi/">Geek blog</a></li>
-<li><a href="https://ryan.hellyer.kiwi/">Personal blog</a></li>
-<li><a href="https://twitter.com/ryanhellyer/">Twitter</a></li>
-</ul>
-</div>
-		</aside>
-	</div>
-
-</footer><!-- #site-footer -->
-
-</body>
-</html>
+  <section class="accent">
+    <div class="wrapper">
+      <h1>WordPress Spam Destroyer</h1>
+      <p>Stops automated spam while remaining as unobtrusive as possible to regular commenters. Simply install, and enjoy a spam free website. Perfect if you're sick of spam. Works via JavaScript with cookies and hidden fields to block bots without bothering real users.</p>
+      <p>
+        <a href="https://wordpress.org/plugins/spam-destroyer/" class="button">Download Spam Destroyer WordPress plugin</a>
+      </p>
+    </div>
+  </section>
+@endsection
