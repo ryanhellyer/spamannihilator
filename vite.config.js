514d8437c6ab33afde0866c1a35fbe5a2521ecd8
Ryan Helyer <r.hellyer@syde.com>
2025-12-06 16:56:45 +0100

    x

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { resolve } from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [],
             refresh: true,
         }),
         tailwindcss(),
